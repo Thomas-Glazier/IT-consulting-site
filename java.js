245c0ca616c9a1d4560e30f036ea5cd2d6af4e99
Thomas-Glazier <tglazier1999@gmail.com>
2021-07-29 19:33:45 -0400

    added images and media query

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
       function sendMail() {
         var link = "mailto:tglazier1999@gmail.com"
-        + "?cc=tglazier1999@gmail.com"
-                 + "&subject=" + encodeURIComponent("Inquiry")
+       
+                 + "?subject=" + encodeURIComponent("Inquiry")
                  + "&body=" + encodeURIComponent(document.getElementById('myText').value)
         ;
         
