a4b3d550aa62619c5d7e67e26bdb61fdaced6aef
Thomas-Glazier <tglazier1999@gmail.com>
2021-07-29 20:07:14 -0400

    added validation

--- java.js
+++ java.js
@@ -8,9 +8,10 @@
         window.location.href = link;
     }
     function validate() {
-      var val = document.getElementById('textarea').value;
+      var val = document.getElementById('myText').value;
   
       if (/^\s*$/g.test(val) || val.indexOf('\n') != -1) {
-          alert('You seem to have forgotten something!');
-      }
-  }
+          alert('You seem to have forgotten something!');  
+        }
+      else {sendMail()}
+    }
