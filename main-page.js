9e54898b6384eed2f27f7405d91837730d9b94f4
Thomas-Glazier <tglazier1999@gmail.com>
2021-08-05 23:35:01 -0400

    Added comments to code

--- main-page.js
+++ main-page.js
@@ -1,17 +1,19 @@
+//opens mail app on PC//
       function sendMail() {
         var link = "mailto:tglazier1999@gmail.com"
-       
+
                  + "?subject=" + encodeURIComponent("Inquiry")
                  + "&body=" + encodeURIComponent(document.getElementById('myText').value)
         ;
-        
+
         window.location.href = link;
     }
+// checks for text in box//
     function validate() {
       var val = document.getElementById('myText').value;
-  
+
       if (/^\s*$/g.test(val) || val.indexOf('\n') != -1) {
-          alert('You seem to have forgotten something!');  
+          alert('You seem to have forgotten something!');
         }
       else {sendMail()}
     }
